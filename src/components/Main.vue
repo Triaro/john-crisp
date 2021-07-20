e0528205fd00a68c1cb881a643165c40299af94c
Triaro <abhimehra2762@gmail.com>
2021-07-21 01:02:20 +0530

    .

---
+++ src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex  w-full p-3">
+    <div class="left w-full">
+      <div class="flex justify-left">
+        <button class="px-3 py-1 rounded bg-gray-300" >
+          <font-awesome-icon icon="arrow-left" />
+          Back
+        </button>        
+      </div>
+      <h2>
+          Hell
+      </h2>
+    </div>
+    <div class="right w-full">
+      <p class="flex justify-left">Card List (1)</p>
+    </div>
+    
+  </div>
+</template>
+
+<script> 
+export default {
+  name: 'Main',
+  props: {
+   
+  }
+}
+</script>
+
+<style>
+  @import '../styles/main.css';
+</style>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '../public/index.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faArrowLeft)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
